e34a545dd3c2a6a560b6d725ce60768dae99d93f
Vinny <vinnyfiore@users.noreply.github.com>
2017-04-21 20:04:29 -0400

    Added cars

--- app.py
+++ app.py
@@ -23,6 +23,36 @@ cars = [
 	},
 	
 	{'id' : '3', 
+	 'make' : 'Audi', 
+	 'model' : 'R8',
+	 'year' : '2017',
+	 'engine' : 'V10',
+	 'engine displacement' : '5.2L',
+	 'horsepower' : '542hp',
+	 'torque' : '294lb-ft'
+	},
+	
+	{'id' : '4', 
+	 'make' : 'Bentley', 
+	 'model' : 'Mulsanne',
+	 'year' : '2017',
+	 'engine' : 'V8',
+	 'engine displacement' : '6.8L',
+	 'horsepower' : '498hp',
+	 'torque' : '555lb-ft'
+	},
+	
+	{'id' : '5', 
+	 'make' : 'McLaren', 
+	 'model' : '650S Spider',
+	 'year' : '2017',
+	 'engine' : 'V8',
+	 'engine displacement' : '3.8L',
+	 'horsepower' : '632hp',
+	 'torque' : '369lb-ft'
+	},
+	
+	{'id' : '6', 
 	 'make' : 'Mazda', 
 	 'model' : 'RX7',
 	 'year' : '1994',
@@ -30,6 +60,46 @@ cars = [
 	 'engine displacement' : '1.3L',
 	 'horsepower' : '255hp',
 	 'torque' : '217lb-ft'
+	},
+	
+	{'id' : '7', 
+	 'make' : 'Rolls-Royce', 
+	 'model' : 'Ghost',
+	 'year' : '2017',
+	 'engine' : 'V12',
+	 'engine displacement' : '6.6L',
+	 'horsepower' : '555hp',
+	 'torque' : '424lb-ft'
+	},
+	
+	{'id' : '8', 
+	 'make' : 'Subaru', 
+	 'model' : 'BRZ',
+	 'year' : '2017',
+	 'engine' : 'Flat 4',
+	 'engine displacement' : '2.0L',
+	 'horsepower' : '197hp',
+	 'torque' : '111lb-ft'
+	},
+	
+	{'id' : '9', 
+	 'make' : 'Toyota', 
+	 'model' : 'Celica',
+	 'year' : '2005',
+	 'engine' : 'I4',
+	 'engine displacement' : '1.8L',
+	 'horsepower' : '180hp',
+	 'torque' : '130lb-ft'
+	},
+	
+	{'id' : '10', 
+	 'make' : 'Chevrolet', 
+	 'model' : 'Camaro',
+	 'year' : '1977',
+	 'engine' : 'V8',
+	 'engine displacement' : '5.7L',
+	 'horsepower' : '165hp',
+	 'torque' : '264lb-ft'
 	}	
 ]
 
